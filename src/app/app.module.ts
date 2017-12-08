e3d8accaf77fb2cc2cbd90a06cb4d759d6f9ef74
Mateusz Sajnóg <mateusz.sajnog@britenet.com.pl>
2017-12-08 15:36:58 +0100

    routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,19 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     CoreModule,
+    AppRoutingModule,
     BrowserAnimationsModule,
     AngularFontAwesomeModule
   ],
